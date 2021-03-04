15d5317b7a7f4edc9174230474c86db78b4c377a
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-04 04:00:38 +0100

    revertendo homescren para material

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bfa_turismo/src/screens/home_Screen.dart';
-import 'package:bfa_turismo/src/screens/places_screen.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home:PlacesScreen()
+      home:HomeScreen()
     );
   }
 }
