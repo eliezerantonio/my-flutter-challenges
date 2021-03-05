524ca266d64c7078ed7a1588b9c9378634cc6bf5
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-05 14:47:04 +0100

    controlando animacao

--- lib/src/screens/home_Screen.dart
+++ lib/src/screens/home_Screen.dart
@@ -2,12 +2,21 @@ import 'package:bfa_turismo/src/componets/liquid_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
-class HomeScreen extends StatelessWidget {
-  final controller = LiquidController();
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     pageChangeCallback(int page) {
-      print(page);
+      setState(() {
+        currentPage = page;
+      });
+
+      print(currentPage);
     }
 
     updateTypeCallback(UpdateType updateType) {
@@ -17,11 +26,11 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: LiquidSwipe(
         pages: liquidPages,
-        liquidController: controller,
-        fullTransitionValue: 800,
-        enableLoop: true,
-        positionSlideIcon: 0.8,
+        fullTransitionValue: currentPage != 2 ? 800 : 0,
+        enableLoop: false,
         waveType: WaveType.liquidReveal, //tipos
+        onPageChangeCallback: pageChangeCallback,
+        currentUpdateTypeCallback: updateTypeCallback,
       ),
     );
   }
