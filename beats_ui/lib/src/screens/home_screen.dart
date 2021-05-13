4629f3d856a1ced302ff669a1fb8a871c3faffe0
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-05-13 17:15:16 -0400

    customAppbar

--- beats_ui/lib/src/screens/home_screen.dart
+++ beats_ui/lib/src/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:beats_ui/src/widgets/circular_background.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -7,10 +8,71 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        CircularBackground(),
-      ],
-    ));
+      body: Stack(
+        children: [
+          CircularBackground(),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CustomAppBar(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Row(
+        children: [
+          IconButton(icon: Icon(FontAwesomeIcons.arrowLeft), onPressed: () {}),
+          Spacer(),
+          IconButton(icon: Icon(FontAwesomeIcons.search), onPressed: () {}),
+          Stack(
+            children: [
+              IconButton(
+                  icon: Icon(FontAwesomeIcons.shoppingBag), onPressed: () {}),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                  child: Text(
+                    "1",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+          IconButton(
+              icon: Icon(FontAwesomeIcons.ellipsisV, size: 15),
+              onPressed: () {}),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Container(
+              color: Colors.blue,
+              width: 50,
+              height: 50,
+            ),
+          ),
+          SizedBox(
+            width: 15,
+          )
+        ],
+      ),
+    );
   }
 }
