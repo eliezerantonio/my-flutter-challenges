02217179527b9b4891a4cb25b888e207dbb3888b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-12-15 21:26:39 +0100

    done

--- movie_app/lib/screens/details_movie_screen.dart
+++ movie_app/lib/screens/details_movie_screen.dart
@@ -36,9 +36,9 @@ class DetailsMovieScreen extends StatelessWidget {
               ),
               Positioned(
                 bottom: 0,
-                child: AnimatedContainer(
+                child: Container(
                   width: size.width,
-                  height: size.height * 0.7,
+                  height: size.height * 0.8,
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(20),
@@ -46,20 +46,22 @@ class DetailsMovieScreen extends StatelessWidget {
                     ),
                     color: Colors.white,
                   ),
-                  duration: const Duration(milliseconds: 400),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const SizedBox(
                         height: 20,
                       ),
-                      Text(
-                        movie["title"],
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey[850],
-                          fontWeight: FontWeight.bold,
+                      Center(
+                        child: Text(
+                          movie["title"],
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.grey[850],
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                       const SizedBox(height: 7),
@@ -74,69 +76,47 @@ class DetailsMovieScreen extends StatelessWidget {
                         ],
                       ),
                       const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FadeInUpBig(
-                            duration: const Duration(milliseconds: 500),
-                            child: const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 18,
-                            ),
-                          ),
-                          FadeInUpBig(
-                            delay: const Duration(milliseconds: 2),
-                            duration: const Duration(milliseconds: 750),
-                            child: const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 18,
-                            ),
-                          ),
-                          FadeInUpBig(
-                            delay: const Duration(milliseconds: 2),
-                            duration: const Duration(milliseconds: 1200),
-                            child: const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 18,
-                            ),
-                          ),
-                          FadeInUpBig(
-                            delay: const Duration(milliseconds: 2),
-                            duration: const Duration(milliseconds: 1450),
-                            child: const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 18,
-                            ),
-                          ),
-                          FadeInUpBig(
-                            delay: const Duration(milliseconds: 2),
-                            duration: const Duration(milliseconds: 1750),
-                            child: const Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                              size: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(movie["diretor"]),
-                      Expanded(
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: 3,
-                          itemBuilder: (_, index) {
-                            final m = movie["actores"][index];
-                            return _actorCartao(m);
-                          },
-                          shrinkWrap: true,
+                      classification(),
+                      Center(child: Text(movie["diretor"])),
+                      const SizedBox(height: 20),
+                      FadeInUpBig(
+                        delay: const Duration(milliseconds: 3),
+                        duration: const Duration(milliseconds: 2000),
+                        child: const Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 30, vertical: 4),
+                          child: Text("Actors",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              )),
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      const Spacer(),
+                      actors(),
+                      FadeInUpBig(
+                        delay: const Duration(milliseconds: 3),
+                        duration: const Duration(milliseconds: 2000),
+                        child: const Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 30, vertical: 4),
+                          child: Text("History",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        child: FadeInUpBig(
+                          delay: const Duration(milliseconds: 3),
+                          duration: const Duration(milliseconds: 2000),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 30),
+                            child: Text(
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliter homines, aliter philosophos loqui putas oportere? Urgent tamen et nihil remittunt. Aut unde est hoc contritum vetustate proverbium: quicum in tenebris? Facit enim ille duo seiuncta ultima bonorum, quae ut essent vera, coniungi debuerunt; Neque solum ea communia, verum etiam paria esse dixerunt. Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus? Et hercule-fatendum est enim, quod sentio -mirabilis est apud illos contextus rerum. Duo Reges: constructio interrete."),
+                          ),
+                        ),
+                      ),
                       Container(
                         width: MediaQuery.of(context).size.width,
                         margin: const EdgeInsets.symmetric(horizontal: 30),
@@ -159,23 +139,99 @@ class DetailsMovieScreen extends StatelessWidget {
       ),
     );
   }
+
+  Expanded actors() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (_, index) {
+            final image = movie["images"][index];
+            final mov = movie["actores"][index];
+            return FadeInUpBig(
+              delay: const Duration(milliseconds: 3),
+              duration: const Duration(milliseconds: 2000),
+              child: actorCard(mov, image),
+            );
+          },
+          shrinkWrap: true,
+        ),
+      ),
+    );
+  }
+
+  Row classification() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FadeInUpBig(
+          duration: const Duration(milliseconds: 500),
+          child: const Icon(
+            Icons.star,
+            color: Colors.amber,
+            size: 18,
+          ),
+        ),
+        FadeInUpBig(
+          delay: const Duration(milliseconds: 2),
+          duration: const Duration(milliseconds: 750),
+          child: const Icon(
+            Icons.star,
+            color: Colors.amber,
+            size: 18,
+          ),
+        ),
+        FadeInUpBig(
+          delay: const Duration(milliseconds: 2),
+          duration: const Duration(milliseconds: 1200),
+          child: const Icon(
+            Icons.star,
+            color: Colors.amber,
+            size: 18,
+          ),
+        ),
+        FadeInUpBig(
+          delay: const Duration(milliseconds: 2),
+          duration: const Duration(milliseconds: 1450),
+          child: const Icon(
+            Icons.star,
+            color: Colors.amber,
+            size: 18,
+          ),
+        ),
+        FadeInUpBig(
+          delay: const Duration(milliseconds: 2),
+          duration: const Duration(milliseconds: 1750),
+          child: const Icon(
+            Icons.star_border,
+            color: Colors.grey,
+            size: 18,
+          ),
+        ),
+      ],
+    );
+  }
 }
 
-Widget _actorCartao(actor) {
+Widget actorCard(actor, image) {
   return Container(
+    margin: EdgeInsets.only(left: 10),
     child: Column(
       children: <Widget>[
         ClipRRect(
           borderRadius: BorderRadius.circular(20.0),
-          child: const FadeInImage(
-            placeholder: AssetImage('assets/img/no-image.jpg'),
-            height: 150.0,
+          child: FadeInImage(
+            placeholder: const AssetImage('assets/no-image.jpg'),
+            height: 100.0,
             fit: BoxFit.cover,
             image: NetworkImage(
-              "actor.getFoto()",
+              image,
             ),
           ),
         ),
+        SizedBox(height: 7),
         Text(
           actor,
           overflow: TextOverflow.ellipsis,
--- movie_app/lib/screens/movies_screen.dart
+++ movie_app/lib/screens/movies_screen.dart
@@ -104,7 +104,7 @@ class _MoviesScreenState extends State<MoviesScreen> {
           Navigator.push(
             context,
             PageRouteBuilder(
-              transitionDuration: const Duration(milliseconds: 300),
+              transitionDuration: const Duration(milliseconds: 1000),
               pageBuilder: (_, __, ___) => DetailsMovieScreen(
                 movie: movie,
               ),
@@ -244,6 +244,15 @@ class _MoviesScreenState extends State<MoviesScreen> {
         'Tom Halland',
         'Zendaya',
         'Willem Dafoe',
+        'Jamie Foxx',
+        'Benedict Wong'
+      ],
+      'images': [
+        'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT2mdIv2oSgUO0zm7jZHboIgb1T7ligSAizsDiuDbOC94Dt8TZffj5WtNHFvoci',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPp7UQ5G0rhPhv6YmpJaW_10JHbxI7sdzcYIEQGTrVPxhBcxV9',
+        'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRFYlQPvEXywuwkAHN4V9hucf0gflp0DURrHByQ9ijoDvb5ZzWw',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6FGrcpWKJc-y8maa2E5u33sWL4KQXBV2FtNmaV9jfih5GJ0e',
+        'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRhgeAN_W918Iv5fx1WeJMFiNwsePWLQNMq8yV2X-fR1uCxt-iWUVhd701ySYUj'
       ],
       'image':
           'https://www.moviepostersgallery.com/wp-content/uploads/2021/12/Spidermannowayhome2.jpg',
@@ -257,6 +266,15 @@ class _MoviesScreenState extends State<MoviesScreen> {
         'Scarlett Johansson',
         'Florence Pugh',
         'Rachel Weisz',
+        'David Harbour',
+        'Olga Kurylenko'
+      ],
+      'images': [
+        'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR12u5Cc7lls-tDFSjb-j0ules8_c6lrXgXkDUkDgAaJbbB4Df28b-WEXGS-G3_',
+        'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcSmNt8UEaTX5t3kN4G0I8kQikhWdc0Aj1FbX6_9XlK9UOKjHaoCZRfz50AFO3jN',
+        'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSQNEbvHBIFFng3NAanlhlOfLlGkLuRWLsKNqhkpp3qy9gpTo_XkZt_pj9e0v3U',
+        'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSw-hcpWMQGl_wSd0y5riySoGZ19UUAaX7alKnkJoK6XEv6YVwBVtbc_HaLr5E4',
+        'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQhD399rEwQSPP6VdbYUAxCA_G8Bd9j6qWKwzcNu4pivgAMiV7Y1OUYYSSNnq7a'
       ],
       'image':
           'https://www.moviepostersgallery.com/wp-content/uploads/2020/08/Blackwidow2.jpg',
@@ -270,6 +288,14 @@ class _MoviesScreenState extends State<MoviesScreen> {
         'Lana Wachowski',
         'Carrie-Anne Moss',
         'Priyanka Chopra',
+        'Jessica Henwick','Daniel Bernhardt'
+      ],
+      'images': [
+        'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRHpx4nrlQmqA83ImfeOvsRKcrB2NN15NHAvYWpBWobbDe0t8Fkza7gfUEX3GtA',
+        'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQowPUlZWy6FI3GG7mUhkgbcwd1bILfmgI9m-36EijHHeEDuyY-TvogECI7AAaZ',
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ9O5SB_crSVtojJYleoSjb70FrpRW6ZOC61P-77hsBcjOADMSt',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ9A34J_XmZAfxsgRqheKnaKutKP1iVH4BFCXx9rwL6xUthgC_f',
+        'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcS3cr_WI7Ak6eOvjStCAKOn7dJC2pDSi-Q5uX07TSeTElfu7QHKVXCJC1IO3ImD'
       ],
       'image':
           'https://www.moviepostersgallery.com/wp-content/uploads/2021/12/Matrixresurrections1.jpg',
@@ -282,7 +308,16 @@ class _MoviesScreenState extends State<MoviesScreen> {
       'actores': [
         'Simu Liu',
         'Awkwafina',
-        'Tony Leung Chiu-Wai',
+        'Tony Leung ',
+        'Meng er Zhang',
+        'Fala Chen'
+      ],
+      'images': [
+        'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSgc0EgUR4uEW5ECnQc3HoVZifhaTlSlgEuDO8aBM8D_EDcUeBS9T_zNdi1wQAF',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT0S9eaXPr3G91_p6w1dv7bmHSaIhhbqyaDsZOLrqfNCZY6BX2H',
+        'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTw6daeGWxFQUsK2oF_8uvj7UjQaycuxgTBv8KJe02BWHLd0cMa80D_R1ftfy4g',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRUQ_vZq40PsWRCi1GSQhvPBwMhHrHFsQLRYEKo-cQZ-R45cbbz',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCKMLepo51o79HX_ud6Ab8qqbKMaT_7U2y26cDsVJCF2FOJJC7'
       ],
       'image':
           'https://www.moviepostersgallery.com/wp-content/uploads/2021/09/Shangchi5.jpg',
